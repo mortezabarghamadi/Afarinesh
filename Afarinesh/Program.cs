d4ec82436e3df7a2b9173246f308a0484721b94c
mortezabarghamadi <mortezabarghamadi5@gmail.com>
2024-12-03 21:15:23 +0330

    first comit

---
+++ Afarinesh/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.PortableExecutable;
+using Tamrin.classes;
+
+List<int> mylist = new List<int>()
+    ;
+Console.WriteLine("please specify the number of entries in the lis");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("please enter the number");
+for (int i = 0; i < n; i++)
+{
+    int adad2 = Convert.ToInt32(Console.ReadLine());
+    mylist.Add(adad2);
+}
+
+MySort mySort = new MySort();
+mySort.bubllesort(mylist, n);
+foreach (var temp in mylist)
+{
+    Console.WriteLine($"number:{temp}");
+}
---
+++ Afarinesh/classes/Mysort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Tamrin.classes
+{
+    public  class MySort
+    {
+        public void bubllesort(List<int> mylist,int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                for (int j = 0; j < n-i+1; j++)
+                {
+                    if (mylist[j] > mylist[j + 1])
+                    {
+                        int temp =mylist[j];
+                        mylist[j] = mylist[j + 1];
+                        mylist[j + 1] = temp;
+                    }
+                    
+                }
+            }
+        }
+
+        
+
+    }
+}
